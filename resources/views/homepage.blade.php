c16689c728562ad6cf738cdc6cc10268f4439b65
ryanthony6 <exoticfire5@gmail.com>
2024-06-10 01:28:02 +0700

    add contact-us section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -66,7 +66,25 @@
     </div>
 </section>
 
-
+<section id="contact-us">
+    <div class="container">
+        <h2 class="text-center pt-5 pb-4">Contact Us</h2>
+        <div class="row">
+            <div class="col-md-6">
+                <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control" id="message" rows="3"></textarea>
+    </div>
+</section>
 
 @endsection
 
