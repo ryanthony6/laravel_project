465def8fec3670ef32e6f0fc8994f459692e058f
ryanthony6 <exoticfire5@gmail.com>
2024-06-25 12:19:43 +0700

    add new services for homepage and remove booking court

---
+++ resources/views/icons/userfriendly.blade.php
@@ -0,0 +1,6 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 24 24" fill="none" stroke="#028a00"
+    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round-check mb-4 text-primary">
+    <path d="M2 21a8 8 0 0 1 13.292-6" />
+    <circle cx="10" cy="8" r="5" />
+    <path d="m16 19 2 2 4-4" />
+</svg>
